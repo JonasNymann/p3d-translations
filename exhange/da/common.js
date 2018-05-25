e6084140f247832188821654cfac803a6230fcdb
JonasNymann <39618462+JonasNymann@users.noreply.github.com>
2018-05-25 14:43:30 +0200

    Create common.js

---
+++ exhange/da/common.js
@@ -0,0 +1,192 @@
+{
+	"masthead": {
+		"holdings": "Beholdning",
+		"balance": "{{balance}} ETH i kontrakten"
+	},
+	"menu": {
+		"exchange" : [
+			"Udveksling",
+			"Sælg & Udbetal",
+			"Overfør",
+			"Beholdning"
+		],
+		"staking": "Staking & Masternode",
+		"news": "Nyheder",
+		"chat": "Snak",
+		"explorer": "Udforsker"
+	},
+	"notifications": {
+		"loading": "Loader...",
+		"cancelled": "Transaktionen blev annulleret af brugeren.",
+		"error": "Der skete en fejl",
+		"withdrawalSubmitted": "udbetalings anmodningen er blevet overført til blockchainen. Afventer bekræftelse..", 
+		"sellSubmitted": "Salgs ordren er blevet overført til blockchainen. Afventer bekræftelse..",
+		"sellConfirmed": "Din P3D token salgs ordre er blevet bekræftet af blockhainen! Du solgte succesfuldt {{tokens}} P3D tokens for {{eth}} ETH.",
+		"sellExternal": "En anden har lige solgt {{tokens}} P3D tokens for {{eth}} ETH.",
+		"buySubmitted": "Købs ordrer er blevet overført til blockchainen. Afventer bekræftelse..",
+		"buyConfirmed": "Din P3D token købs ordrer er blevet bekræftet af blockchainen! Du købte succesfuldt {{tokens}} P3D tokens med {{eth}} ETH.",
+		"buyExternal": "En anden har lige købt {{tokens}} P3D tokens med {{eth}} ETH.",
+		"transferSubmitted": "overførelses ordren er blevet overført til blockchainen. Afventer bekræftelse..",
+		"transferConfirmed": "Din P3D token overførelses ordrer er blevet bekræftet af blockchainen! Du overførte succesfuldt {{tokens}} P3D tokens.",
+		"transferExternal": "En anden har lige overført {{tokens}} P3D tokens."
+	},
+	"footer": "det her er en underholdnings platform, ikke en investerings platform, er du ikke underholdt?",
+	"pages": {
+		"purchase": {
+			"title": "Køb tokens",
+			"item": {
+				"buy": {
+					"title": "Køb P3D tokens",
+					"spend": "Ethereum at bruge",
+					"approx": "Omkring {{amount}} P3D tokens.",
+					"invalid": "Skriv et gyldigt nummer.",
+					"empty": "Skriv et nummer ovenover, for straks at se en estimeret pris.",
+					"button": "Køb",
+					"hint": "1). Desto højere gas pris du tilbydder, desto hurtigere vil ETH minerne opsamle, og bekræfte din transaktion."
+				},
+				"reinvest": {
+					"title": "Reinvester Gauntlet belønninger",
+					"button": "Reinvester!"
+				},
+				"recent": {
+					"title": "Seneste købs ordrer",
+					"date": "Dato.",
+					"tx": "Tx.",
+					"address": "Addresse"
+				}
+			}
+		},
+		"sell": {
+			"title": "Sælg tokens",
+			"item": {
+				"sell": {
+					"title": "Sælg P3D tokens",
+					"spend": "Tokens at sælge",
+					"approx": "Omkring {{amount}} ETH.",
+					"invalid": "Skriv et gyldigt nummer.",
+					"empty": "Skriv et nummer ovenover, for straks at se en estimeret pris.",
+					"button": "Sælg!",
+					"all": "Alt",
+					"hint": "1). Desto højere gas pris du tilbydder, desto hurtigere vil ETH minerne opsamle, og bekræfte din transaktion.",
+					"warning": " Er du sikker på at du vil sælge tokens? Sælge er at indrømme til dig selv, og alle andre at dine hænder ikke længere er stærke nok, til at spille. de 10% i gebyr fra dit salg, vil helt bestemt være værdsat af andre holdere.",
+					"confirm": "JA, JEG HAR SVAGE HÆNDER.",
+					"deny": "NEJ, JEG HAR FUNDET NY STYRKE."
+				},
+				"withdraw": {
+					"title": "Udestående Gauntlet Belønninger",
+					"button": "Udbetal",
+					"reinvest": "Reinvester",
+					"warning": " Vil du udbetale dine Gauntlet belønninger? Vær ikke bekymret, der er ingen gebyrer, Ethen er allerede så godt som din. Men hvis du nyder dig selv (Og måden tingene går) Bliver du måske nødt til at indrømme vanvid, og prøve vores reinvesterings funktion. Flere tokens betyder, at du får en større del af alle gebyrer, der er betalt ind til kontrakten. Det er lidt ligesom at fordoble, du får måske dobbelt, og du får måske ikke noget.",
+					"confirm": "JA, FORTÆL ROBOTEN AT DET ER MIN LØNNINGSDAG.",
+					"deny": "NEJ, JEG KAN LIDE HVORDAN DET HER GÅR."
+				},
+				"recent": {
+					"title": " Seneste salgs ordrer",
+					"date": "Dato.",
+					"tx": "Tx.",
+					"address": "Addresse"
+				}
+			}
+		},
+		"transfer": {
+			"title": "Overfør tokens",
+			"item": {
+				"transfer": {
+					"title": "Overfør P3D tokens",
+					"spend": "Tokens at sende",
+					"recipient": "Modtagerens pung",
+					"invalid": "Ugyldig pung addresse...",
+					"empty": "Ugyldigt beløb af tokens...",
+					"button": "Overfør",
+					"hint": " 1). Husk, der er en fast 10% gebyr på alle tokens overført <br> 2). Dine udestående Gauntlet belønninger vil automatisk, blive udbetalt til din eth pung, når du overføre tokens. Bliv ikke nervøs.<br> 3). Desto højere gas pris du tilbydder, desto hurtigere vil ETH minerne opsamle, og bekræfte din transaktion."
+				},
+				"recent": {
+					"title": "Seneste overførelses ordrer",
+					"date": "Dato.",
+					"tx": "Tx.",
+					"from": "Fra",
+					"to": "Til"
+				}
+			}
+		},
+		"holdings": {
+			"title": "My beholdninger",
+			"item": {
+				"tokens": {
+					"title": "Tokens"
+				},
+				"rewards": {
+					"title": "Belønninger",
+					"reinvest": "(Hvis reinvesteret)"
+				},
+				"portfolio": {
+					"title": "Portefølje vækst",
+					"description": "Det advancerede diagram modul, er i øjeblikket i udvikling."
+				},
+				"actions": {
+					"buy": "Køb tokens",
+					"withdraw": "udbetal",
+					"sell": "Sælg tokens"
+				},
+				"configuration": {
+					"title": "Konfiguration",
+					"description": "Personaliserings indstillinger vil snart komme her, inklusiv den meget efterspurgte natte-tilstand. Har du nogen foreslag til nogen funktioner? <a href='https://discord.gg/wfG97Kt'>Vær sød at besøge os i vores egen gruppe chat.</a>"
+				},
+				"recent": {
+					"title": "Min transaktions historik",
+					"date": "Dato.",
+					"tx": "Tx.",
+					"event": "Hændelse"
+				}
+			}
+		},
+		"staking": {
+			"title": "Staking",
+			"item": {
+				"description": {
+					"title": "Masternoder...",
+					"columns": [
+						{
+							"title": "Udvid dit netværk!",
+							"text": "Hvis din pung holder 5 tokens, vil kontrakten generere et link til din masternode, kontrakten generere et link til din masternode for dig!<br><br> Når nogen besøger siden via dette unikke link, har de din masternode gemt i en cookie, der tracker alle køb af P3D de laver, nu og i fremtiden."
+						},
+						{
+							"title": "Hvad er der i det for mig?",
+							"text": "<b>Ethereum!</b> Når nogen køber med dit masternode link, <strong>Modtager du med det samme 33% af købs gebyren, de ellers skulle have betalt til kontrakten, i ETH</strong> - ingen spørgsmål spurgt, ingen besvær."
+						}
+					]
+				},
+				"masternode": {
+					"title": "Mit masternode link",
+					"link": "Dit unikke masternode link er...",
+					"pointers": "Generale henvisninger for at udvide dit eget netværk:",
+					"list": [
+						"Prøv at henvænd dig til andre medlemmer, i fælleskaber du er bekændt med",
+						"Snak om powh.io i forumer på dit eget sprog for maximal potentiale.",
+						"Anvend <a href='https://powh.io/shill-kit.html'>shill-kit, er designet til at få folk til at klikke på dit link!</a>",
+						"Vær ærlig. del konceptet bag PoWH, og hvad du føler om projektet.",
+						"Lad vær med at lyve eller love garanteret afkast.",
+						"Lad vær med kun at spamme dit masternode link. Snak om projektet istedet.",
+						"Undladelse af at forpligte sig til de ovestående regler, kan resultere i suspension af dit masternode link.",
+						"Masternode staking kravene ændres måske over tid, så vær sikker og reinvester dit udbytte.",
+						"I tilfælde af at du føler dig klog. at bruge sit eget masternode link virker ikke"
+					]
+				},
+				"tracker": {
+					"title": "tracker visninger...",
+					"text": "samlet antal masternode besøg <br>(Virker ikke endnu)"
+				},
+				"earnings": {
+					"text": "Udestående masternode belønninger"
+				},
+				"recent": {
+					"title": "Din masternode konvetering",
+					"date": "Dato.",
+					"tx": "Tx.",
+					"address": "Addresse",
+					"profit": "Profit"
+				}
+			}
+		}
+	} 
+}
